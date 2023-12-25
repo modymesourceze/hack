7a66bf9791aec8c3dce95fce3e113eb046ae4a2b
modymesourceze <148470115+modymesourceze@users.noreply.github.com>
2023-12-25 09:13:02 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", 25281175))
+
+    API_HASH = os.environ.get("API_HASH", "6d99cb2b60a2c519fc1f99bd19565730")
+    
+    BOT_USER = os.environ.get("BOT_USER", "")
+    
+    USER_ID = os.environ.get("USER_ID", "")
